4defecdeb96705baae7411f00ccfba5d94e6f9e5
Egor <ururugeorge@gmail.com>
2026-02-15 18:40:03 +0300

    feat: added timeline layout skeleton

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -4,6 +4,23 @@ export function Timeline() {
 	return (
 		<section className='timeline'>
 			<h2 className='timeline__title'>Timeline block</h2>
+
+			<div className='timeline__years'>
+				<span className='timeline_year'>2015</span>
+				<span className='timeline_year'>2022</span>
+			</div>
+
+			<div className='timeline__circle-area'>
+				<button className='timeline__arrow'>&#8592;</button>
+
+				<div className='timeline__circle'>circle placeholder</div>
+
+				<button className='timeline__arrow'>&#8594;</button>
+			</div>
+
+			<div className="timeline__slider-placeholder">
+				slider placeholder
+			</div>
 		</section>
 	)
 }
