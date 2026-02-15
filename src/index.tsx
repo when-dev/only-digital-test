55ff0e594148251f179684acd10254ede3614b80
Egor <ururugeorge@gmail.com>
2026-02-15 18:28:50 +0300

    setup: removed app structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { App } from './app/App'
+import { App } from './App'
 import "./styles/global.scss"
 
 createRoot(document.getElementById('root')!).render(<App />)
