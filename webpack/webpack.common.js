a8c4597ec36bef399f075d0a50e4890538dfbeac
Egor <ururugeorge@gmail.com>
2026-02-15 18:22:13 +0300

    fix: webpack common config for scss

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -20,16 +20,16 @@ module.exports = {
 		}),
 	],
 	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-		],
-	},
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+	}
 }
